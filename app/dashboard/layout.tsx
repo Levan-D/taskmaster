90dfcf45aeaa60c401ee913dbcd9c3204b1bc1ee
Levan D <97022558+Levan-D@users.noreply.github.com>
2023-09-01 10:36:55 +0400

    a

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -7,8 +7,8 @@ import LogoutBtn from "../components/LogoutBtn"
 import { checkUserExists } from "../actions"
 
 export default async function layout({ children }: { children: React.ReactNode }) {
-  await checkUserExists()
-
+  
+  
   return (
     <div className="flex min-h-screen">
       <Sidebar LogoutBtn={<LogoutBtn />} UserInfo={<UserInfo />} />
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -4,5 +4,5 @@ import { redirect } from "next/navigation"
 import Link from "next/link"
 
 export default function Dashboard() {
-  return <Link href={"/dashboard/today"}></Link>
+  redirect("/dashboard/today")
 }
