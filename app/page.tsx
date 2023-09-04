81fafdfa9c5bc3ea05cbae2052979994d88b97d8
Levan D <97022558+Levan-D@users.noreply.github.com>
2023-09-04 22:39:09 +0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
       <div className="flex gap-4 items-center w-fit mx-auto">
         <>
           <LoginLink className="bg-[#3377ff] text-white px-4 py-2 rounded">
-            Sign in
+            Sign in z
           </LoginLink>
           <RegisterLink className="bg-[#3377ff] text-white px-4 py-2 rounded">
             Sign up
