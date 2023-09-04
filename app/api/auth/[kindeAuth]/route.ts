01f46371712ea669e9692a6e4c78a814b8a33741
Levan D <97022558+Levan-D@users.noreply.github.com>
2023-09-04 12:06:15 +0400

    a

--- app/api/auth/[kindeAuth]/route.ts
+++ app/api/auth/[kindeAuth]/route.ts
@@ -1,8 +1,9 @@
+/** @format */
+
 import { NextRequest } from "next/server"
 import { handleAuth } from "@kinde-oss/kinde-auth-nextjs/server"
 
 export async function GET(request: NextRequest, { params }: any) {
-    // const endpoint = params.kindeAuth
-    // return handleAuth(request, endpoint)
-    return 'hi'
+  const endpoint = params.kindeAuth
+  return handleAuth(request, endpoint)
 }
--- app/api/protected/route.ts
+++ app/api/protected/route.ts
@@ -4,15 +4,13 @@ import { NextResponse } from "next/server"
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server"
 
 export async function GET() {
-  // const { getUser, isAuthenticated } = getKindeServerSession()
+  const { getUser, isAuthenticated } = getKindeServerSession()
 
-  // if (!isAuthenticated()) {
-  //   return new Response("Unauthorized", { status: 401 })
-  // }
-  // const user = getUser()
-  // const data = { message: "Hello User", id: user.id }
+  if (!isAuthenticated()) {
+    return new Response("Unauthorized", { status: 401 })
+  }
+  const user = getUser()
+  const data = { message: "Hello User", id: user.id }
 
-  // return NextResponse.json({ data })
-
-  return 'ja'
+  return NextResponse.json({ data })
 }
