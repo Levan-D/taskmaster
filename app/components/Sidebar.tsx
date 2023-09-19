44b00334cc975280a8f88a44143995cc4720e70b
Levan D <97022558+Levan-D@users.noreply.github.com>
2023-09-19 12:17:48 +0400

    Update Sidebar.tsx

--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -74,62 +74,64 @@ const taskPages: TaskPageType[] = [
   },
 ]
 
-function Desktop({ UserInfo, LogoutBtn }: Props) {
-  const [isOpen, setIsOpen] = useState(true)
-  const pathname = usePathname()
-
-  const toggleNav = () => {
-    setIsOpen(x => !x)
-  }
-
-  const items: DropDownItemType = [
-    {
-      title: "test",
-      action: () => console.log(`alo`),
-    },
-    {
-      break: true,
-    },
-    {
-      JSX: LogoutBtn,
-    },
-  ]
-
+function Footer({ width, isOpen }: { width: number; isOpen: boolean }) {
   return (
-    <div className={` ${isOpen ? "w-[200px] " : "w-[56px]"} shrink-0`}>
-      <nav
-        className={` ${
-          isOpen ? "w-[200px] " : "w-[56px]"
-        }  bg-neutral-950  h-screen fixed  duration-300   flex flex-col  justify-between select-none `}
-      >
-        <div className={`${isOpen && "flex gap-1"}`}>
-          <div className="grow">
-            {isOpen && (
-              <DropdownMenu
-                menuClassName="translate-x-12"
-                items={items}
-                className={"  grow  btnIcon  m-2 w-full"}
-                button={UserInfo}
-              />
-            )}
-          </div>
-          <button onClick={toggleNav} className="  w-10 h-10 m-2 btnIcon ">
-            <Icon
-              path={mdiChevronRight}
-              className={`  ${isOpen && "rotate-180 "} duration-300 mx-auto`}
-              size={1}
-            />
-          </button>
-        </div>
+    <footer className={`text-xs p-2 text-center `}>
+      {(width > 640 || isOpen) &&
+        (isOpen ? (
+          "© 2023 Levan Dolidze. All Rights Reserved."
+        ) : (
+          <Tooltip position="right" text="2023 Levan Dolidze. All Rights Reserved.">
+            <div className="text-center text-lg">©</div>
+          </Tooltip>
+        ))}
+    </footer>
+  )
+}
 
-        <div>
-          {taskPages.map((page, i) => {
-            if ("icon" in page) {
-              return isOpen ? (
+function Links({
+  isOpen,
+  width,
+  toggleNav,
+  pathname,
+}: {
+  isOpen: boolean
+  width: number
+  toggleNav: () => void
+  pathname: string
+}) {
+  return (
+    <div>
+      {taskPages.map((page, i) => {
+        if ("icon" in page) {
+          return (
+            (width > 640 || isOpen) && // Only render if width > 640 or isOpen is true
+            (isOpen ? (
+              <Link
+                key={i}
+                onClick={width <= 640 ? toggleNav : undefined}
+                href={page.path}
+                className={`${!isOpen && "justify-center w-10"} ${
+                  pathname.includes(
+                    typeof page.path === "string" ? page.path : page.path.pathname
+                  ) && "!bg-neutral-800"
+                } px-2 mx-2 flex gap-2 btnIcon h-10 items-center text-sm`}
+              >
+                <div>
+                  <Icon path={page.icon} size={0.8} />
+                </div>
+                <p>{page.title}</p>
+              </Link>
+            ) : (
+              <Tooltip
+                key={i}
+                text={page.title}
+                className={"translate-x-7 "}
+                position="right"
+              >
                 <Link
-                  key={i}
                   href={page.path}
-                  className={`${!isOpen && "justify-center w-10"} ${
+                  className={`${!isOpen && "justify-center w-10"}  ${
                     pathname.includes(
                       typeof page.path === "string" ? page.path : page.path.pathname
                     ) && "!bg-neutral-800"
@@ -138,148 +140,21 @@ function Desktop({ UserInfo, LogoutBtn }: Props) {
                   <div>
                     <Icon path={page.icon} size={0.8} />
                   </div>
-                  <p>{page.title}</p>
                 </Link>
-              ) : (
-                <Tooltip
-                  key={i}
-                  text={page.title}
-                  className={"translate-x-7 "}
-                  position="right"
-                >
-                  <Link
-                    href={page.path}
-                    className={`${!isOpen && "justify-center w-10"}  ${
-                      pathname.includes(
-                        typeof page.path === "string" ? page.path : page.path.pathname
-                      ) && "!bg-neutral-800"
-                    } px-2 mx-2 flex gap-2 btnIcon h-10 items-center text-sm`}
-                  >
-                    <div>
-                      <Icon path={page.icon} size={0.8} />
-                    </div>
-                  </Link>
-                </Tooltip>
-              )
-            }
+              </Tooltip>
+            ))
+          )
+        }
 
-            if ("break" in page) {
-              return (
-                <hr key={i} className="mx-4 my-1 border-t-[1px] border-neutral-800" />
-              )
-            }
-          })}
-        </div>
-
-        <div></div>
-
-        <footer className={`text-xs p-2 text-center `}>
-          {isOpen ? (
-            "© 2023 Levan Dolidze. All Rights Reserved."
-          ) : (
-            <Tooltip position="right" text="2023 Levan Dolidze. All Rights Reserved.">
-              <div className="text-center text-lg">©</div>
-            </Tooltip>
-          )}
-        </footer>
-      </nav>
+        if ("break" in page) {
+          return (
+            (width > 640 || isOpen) && ( // Only render if width > 640 or isOpen is true
+              <hr key={i} className="mx-4 my-1 border-t-[1px] border-neutral-800" />
+            )
+          )
+        }
+      })}
     </div>
-  )
-}
-
-function Mobile({ UserInfo, LogoutBtn }: Props) {
-  const [isOpen, setIsOpen] = useState(true)
-  const pathname = usePathname()
-
-  const toggleNav = () => {
-    setIsOpen(x => !x)
-  }
-
-  const items: DropDownItemType = [
-    {
-      title: "test",
-      action: () => console.log(`alo`),
-    },
-    {
-      break: true,
-    },
-    {
-      JSX: LogoutBtn,
-    },
-  ]
-
-  return (
-    <>
-      <div
-        className={` ${
-          isOpen ? "visible opacity-40" : "collapse opacity-0"
-        }  fixed top-0 right-0 bottom-0 left-0 z-30 bg-neutral-950 duration-300`}
-        onMouseDown={toggleNav}
-      ></div>
-      <nav
-        className={` ${
-          isOpen ? "w-[300px]  bg-neutral-950" : "w-[56px]"
-        }   z-50  h-screen fixed  duration-300   flex flex-col  justify-between select-none `}
-      >
-        <div className={`${isOpen && "flex gap-1"}`}>
-          <div className="grow">
-            {isOpen && (
-              <DropdownMenu
-                menuClassName="translate-x-12"
-                items={items}
-                className={"  grow  btnIcon  m-2 w-full"}
-                button={UserInfo}
-              />
-            )}
-          </div>
-          <button onClick={toggleNav} className="  w-10 h-10 m-2 btnIcon ">
-            <Icon
-              path={mdiChevronRight}
-              className={`  ${isOpen && "rotate-180 "} duration-300 mx-auto`}
-              size={1}
-            />
-          </button>
-        </div>
-
-        <div>
-          {taskPages.map((page, i) => {
-            if ("icon" in page) {
-              return (
-                isOpen && (
-                  <Link
-                    key={i}
-                    onClick={toggleNav}
-                    href={page.path}
-                    className={`${!isOpen && "justify-center w-10"} ${
-                      pathname.includes(
-                        typeof page.path === "string" ? page.path : page.path.pathname
-                      ) && "!bg-neutral-800"
-                    } px-2 mx-2 flex gap-2 btnIcon h-10 items-center text-sm`}
-                  >
-                    <div>
-                      <Icon path={page.icon} size={0.8} />
-                    </div>
-                    <p>{page.title}</p>
-                  </Link>
-                )
-              )
-            }
-
-            if ("break" in page) {
-              return (
-                <hr key={i} className="mx-4 my-1 border-t-[1px] border-neutral-800" />
-              )
-            }
-          })}
-        </div>
-
-        <div></div>
-
-        <footer className={`text-xs p-2 text-center `}>
-          {isOpen && "© 2023 Levan Dolidze. All Rights Reserved."}
-        </footer>
-      </nav>
-    </>
   )
 }
 
@@ -298,9 +173,65 @@ export default function Sidebar({ UserInfo, LogoutBtn }: Props) {
     }
   }, [])
 
-  return width >= 640 ? (
-    <Desktop UserInfo={UserInfo} LogoutBtn={LogoutBtn} />
-  ) : (
-    <Mobile UserInfo={UserInfo} LogoutBtn={LogoutBtn} />
+  const toggleNav = () => {
+    setIsOpen(x => !x)
+  }
+
+  const items: DropDownItemType = [
+    {
+      title: "test",
+      action: () => console.log(`alo`),
+    },
+    {
+      break: true,
+    },
+    {
+      JSX: LogoutBtn,
+    },
+  ]
+
+  return (
+    <div className={` ${isOpen ? "sm:w-[200px] " : "sm:w-[56px]"} w-0 shrink-0`}>
+      {width <= 640 && (
+        <div
+          className={` ${
+            isOpen ? "visible opacity-40" : "collapse opacity-0"
+          }  fixed top-0 right-0 bottom-0 left-0 z-30 bg-neutral-950 duration-300`}
+          onMouseDown={toggleNav}
+        ></div>
+      )}
+
+      <nav
+        className={` ${
+          isOpen ? "w-[200px] bg-neutral-950" : "w-[56px]"
+        }    h-screen fixed sm:bg-neutral-950  duration-300   z-50  flex flex-col  justify-between select-none `}
+      >
+        <div className={`${isOpen && "flex gap-1"}`}>
+          <div className="grow">
+            {isOpen && (
+              <DropdownMenu
+                menuClassName="translate-x-12"
+                items={items}
+                className={"  grow  btnIcon  m-2 w-full"}
+                button={UserInfo}
+              />
+            )}
+          </div>
+          <button onClick={toggleNav} className="  w-10 h-10 m-2 btnIcon ">
+            <Icon
+              path={mdiChevronRight}
+              className={`  ${isOpen && "rotate-180 "} duration-300 mx-auto`}
+              size={1}
+            />
+          </button>
+        </div>
+
+        <Links width={width} isOpen={isOpen} pathname={pathname} toggleNav={toggleNav} />
+
+        <div></div>
+
+        <Footer width={width} isOpen={isOpen} />
+      </nav>
+    </div>
   )
 }
