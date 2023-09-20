56e76b0940dabc60f882d89ad4833101bfee8ee2
Levan D <97022558+Levan-D@users.noreply.github.com>
2023-09-20 14:55:28 +0400

    viewport disableauto zoom

--- app/components/Loader.tsx
+++ app/components/Loader.tsx
@@ -5,7 +5,7 @@ import React from "react"
 export default function Loader() {
   return (
     <div className="w-fit mx-auto min-h-screen flex flex-col justify-center   ">
-      <div className="loader"></div>
+      <div className="loader !sm:w-[250px]  !sm:h-[250px] !w-[125px]  !h-[125px] "></div>
     </div>
   )
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,13 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <Providers>
       <html lang="en">
+        <head>
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, maximum-scale=1"
+          />
+        </head>
+
         <body
           className={`${inter.className}  text-white bg-neutral-900 flex flex-col min-h-screen `}
         >
