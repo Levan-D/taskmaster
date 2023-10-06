f915bc310575ed3b2418f254d4ff8999c473db33
Levan D <97022558+Levan-D@users.noreply.github.com>
2023-10-06 22:07:13 +0400

    added a logo

--- app/page.tsx
+++ app/page.tsx
@@ -10,20 +10,14 @@ import { redirect } from "next/navigation"
 export default function Home() {
   const { getUser, isAuthenticated } = getKindeServerSession()
 
-  // redirecting to dashboard like this due to middleware bug on deployment 
+  // redirecting to dashboard like this due to middleware bug on deployment
   if (isAuthenticated()) redirect("/dashboard/today")
 
   return (
     <section className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="flex gap-4 items-center w-fit mx-auto">
-        <>
-          <LoginLink className="bg-[#3377ff] text-white px-4 py-2 rounded">
-            Sign in
-          </LoginLink>
-          <RegisterLink className="bg-[#3377ff] text-white px-4 py-2 rounded">
-            Sign up
-          </RegisterLink>
-        </>
+        <LoginLink className="btnPrimary py-2 px-4">Sign in</LoginLink>
+        <RegisterLink className="btnSecondary py-2 px-4">Register</RegisterLink>
       </div>
     </section>
   )
