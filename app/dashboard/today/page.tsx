a54c0ba5518534462e7b3bcf325602593d5ba621
Levan D <97022558+Levan-D@users.noreply.github.com>
2023-10-24 20:10:22 +0400

    Update page.tsx

--- app/dashboard/today/page.tsx
+++ app/dashboard/today/page.tsx
@@ -15,7 +15,7 @@ export default async function Today() {
 
   const tasks = await getTodaysTasks()
   if (!tasks.success) return <span></span>
-  // console.log(tasks.data)
+
   return (
     <section className="  max-w-3xl mx-auto    ">
       {tasks.data !== undefined && <TaskDisplay tasks={tasks.data} />}
