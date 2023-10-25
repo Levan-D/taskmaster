02103600c9cf5df3b70925fbcfdf0cd011846819
Levan D <97022558+Levan-D@users.noreply.github.com>
2023-10-25 14:12:41 +0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -39,7 +39,7 @@ export default function Home() {
             xmlnsXlink="http://www.w3.org/1999/xlink"
             viewBox="0 24 150 28"
             preserveAspectRatio="none"
-            shape-rendering="auto"
+            shapeRendering="auto"
           >
             <defs>
               <path
