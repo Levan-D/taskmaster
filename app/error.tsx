d7cabd828b5159efe1ee4e663fe149776c208a97
Levan D <97022558+Levan-D@users.noreply.github.com>
2023-11-04 23:05:04 +0400

    added error pages

---
+++ app/error.tsx
@@ -0,0 +1,30 @@
+/** @format */
+
+"use client"
+
+import { useEffect } from "react"
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string }
+  reset: () => void
+}) {
+  useEffect(() => {
+    console.error(error)
+  }, [error])
+
+  return (
+    <div className="mx-auto h-screen flex  w-fit flex-col justify-center ">
+      <div className="mainContainer p-4">
+        <h2 className="text-lg  ">Something went wrong!</h2>
+        <p className=""> {`${error?.name}: ${error?.message}`}</p>
+
+        <button className="btnSecondary  w-full  mt-4" onClick={() => reset()}>
+          Refresh Page
+        </button>
+      </div>
+    </div>
+  )
+}
